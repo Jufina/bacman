e593371a93866c57f7faa66736a204d70d034e4b
Julia Samoshchenko <nightrina@gmail.com>
2015-05-24 23:48:00 +0300

    Нам стыдно. Мы даже незамейкили, чтобы посмотреть.

--- src/server/GameEngine.java
+++ src/server/GameEngine.java
@@ -1,7 +1,78 @@
 package server;
 
+import java.io.*;
+
 /**
  * Created by Julia on 24.05.2015.
  */
 public class GameEngine {
+    private byte[][] map;
+    private byte[] teamsScore;
+
+    private void tryToMove(int i, int j, int toI, int toJ) {
+        int teamId = (map[i][j]%2 == 0?1:0);
+        if (map[toI][toJ] == 6 || map[toI][toJ] == 0) {
+            if (map[toI][toJ] == 6) {
+                teamsScore[teamId]++;
+            }
+            map[toI][toJ] = map[i][j];
+            map[i][j] = 0;
+        }
+    }
+
+    public byte[] rebuildingMap(byte[] movepl) {
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; j++) {
+                if (map[i][j] >=1 && map[i][j] <= 4) {
+                    switch (movepl[map[i][j] - 1]) {
+                        case 0: // up
+                            tryToMove(i, j, i - 1, j);
+                            break;
+                        case 1: // right
+                            tryToMove(i, j, i, j + 1);
+                            break;
+                        case 2: // down
+                            tryToMove(i, j, i + 1, j);
+                            break;
+                        case 3: // left
+                            tryToMove(i, j, i, j - 1);
+                            break;
+                    }
+                }
+            }
+        }
+        return getResult();
+    }
+    public byte[] getResult() {
+        byte[] res = new byte[20*20 + 2];
+        for (int i=0; i<20; i++) {
+            for (int j=0; j<20; j++) {
+                res[i*20 + j] = map[i][j];
+            }
+        }
+        res[20*20] = teamsScore[0];
+        res[20*20 + 1] = teamsScore[1];
+        return res;
+    }
+    public GameEngine(int numOfMap) {
+        FileInputStream inputStream;
+        try {
+            inputStream = new FileInputStream("map" + numOfMap + ".txt");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        map = new byte[20][20];
+        try {
+            for (int i = 0; i < 20; i++) {
+                String str = reader.readLine();
+                for (int j = 0; j < 20; j++) {
+                    map[i][j] = (byte) (str.charAt(j) - '0');
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        teamsScore = new byte[2];
+    }
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -2,11 +2,14 @@ package server;
 
 import java.io.*;
 import java.net.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * Created by Julia on 24.05.2015.
+ * Created by Julia Matrokhina on 24.05.2015.
  */
 public class Server {
+    private GameEngine gameEngine;
     public static void main(String[] args) {
         Server server=new Server();
         try {
@@ -15,8 +18,12 @@ public class Server {
             e.printStackTrace();
         }
     }
+   public void Server() {
+       gameEngine=new GameEngine(1);
+   }
     public void run() throws IOException {
-        int pacSize = 1000;
+        Map<Integer, Integer> idPlayers=new HashMap<Integer, Integer>();
+        int pacSize = 20*20+2;
         int port = 4444;
         byte data[] = new byte[pacSize];
         System.out.println("Server starts...");
@@ -29,21 +36,27 @@ public class Server {
             s.setSoTimeout(300000);
             for (int pac = 0; pac < 4; pac++) {
                 s.receive(pacs[pac]);
+                System.out.println("КЛИЕНТ ПОДКЛЮЧИЛСЯ - "+pacs[pac].getPort());
+                idPlayers.put(pacs[pac].getPort(),pac);
             }
 
             for (int pac = 0; pac < 4; pac++) {
+                pacs[pac].setData(gameEngine.getResult());
                 s.send(pacs[pac]);
             }
 
             s.setSoTimeout(600);
             while (true) {
+                byte[] movepl=new byte[4];
                 for (int pac = 0; pac < 4; pac++) {
                     s.receive(pacs[pac]);
+                    movepl[idPlayers.get(pacs[pac].getPort())]=pacs[pac].getData()[0];
                 }
-                //TODO: run Engine
-                //TODO: pacs = ...
+                byte[] result;
+                result=gameEngine.rebuildingMap(movepl);
 
                 for (int pac = 0; pac < 4; pac++) {
+                    pacs[pac].setData(result);
                     s.send(pacs[pac]);
                 }
             }
